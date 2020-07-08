baa3bcb2e2e4049961fa982998528358ad6b0653
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:53:13 -0600

    Add details attribute to Cart

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,9 +1,10 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :details
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.to_i
     @products = []
+    @details = {name: @name, capacity: @capacity}
   end
 
   def add_product(product)
