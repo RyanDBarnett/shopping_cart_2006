5e90a7efd5c429b0821013972dda0297e6a54dc2
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:22:29 -0600

    Add Product not hoarded by default test

--- test/product_test.rb
+++ test/product_test.rb
@@ -36,4 +36,9 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_it_has_not_been_hoarded_by_default
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal false, product.hoarded
+  end
 end
