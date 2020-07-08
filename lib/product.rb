746e8f36bfa701f91e8cd11cad6ee54e63c9f852
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:12:54 -0600

    Add total_price attribute to Product

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,10 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity
+  attr_reader :category, :name, :unit_price, :quantity, :total_price
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @total_price = @quantity * @unit_price
   end
 end
