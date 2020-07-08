02c89a9096929c811139e71922bdab642d2cf75b
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:30:09 -0600

    Add Product hoard method test

--- test/product_test.rb
+++ test/product_test.rb
@@ -41,4 +41,10 @@ class ProductTest < Minitest::Test
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal false, product.is_hoarded?
   end
+
+  def test_it_can_be_hoarded
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product.hoard
+    assert_equal true, product.is_hoarded?
+  end
 end
