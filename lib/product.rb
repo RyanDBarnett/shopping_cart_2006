6bfb0b340190761a6e477dd70bc0da4aa87d79ce
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:23:18 -0600

    Add hoarded attribute to Product

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,11 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity, :total_price
+  attr_reader :category, :name, :unit_price, :quantity, :total_price, :hoarded
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
     @total_price = @quantity * @unit_price
+    @hoarded = false
   end
 end
