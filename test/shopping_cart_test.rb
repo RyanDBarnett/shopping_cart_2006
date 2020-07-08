e2381e5279bd1f0cb0ed7940e2f5361ff32d7a26
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:48:05 -0600

    Add Cart add_product method test

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/product'
 require './lib/shopping_cart'
 
 class ShoppingCartTest < Minitest::Test
@@ -22,5 +23,19 @@ class ShoppingCartTest < Minitest::Test
   def test_it_should_have_no_products_by_default
     cart = ShoppingCart.new("King Soopers", "30items")
     assert_equal [], cart.products
+  end
+
+  def test_it_should_be_able_to_add_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+    toilet_paper = Product.new(:paper, 'toilet paper', 3.70, '10')
+    chicken = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(toilet_paper)
+
+    assert_equal [toilet_paper], cart.products
+
+    cart.add_product(chicken)
+
+    assert_equal [toilet_paper, chicken], cart.products
   end
 end
