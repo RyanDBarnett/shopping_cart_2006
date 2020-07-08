93600e511689460d0c5271a0ff4f59346a24263b
RyanDBarnett <conner2468@gmail.com>
2020-07-08 09:09:52 -0600

    Add unit_price attribute to Product

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,8 @@
 class Product
-  attr_reader :category, :name
+  attr_reader :category, :name, :unit_price
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
+    @unit_price = unit_price
   end
 end
